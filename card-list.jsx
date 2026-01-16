10557c6ccab71bbda2deacb2c3901415af49541a
Ybmzx <111725879+Ybmzx@users.noreply.github.com>
2026-01-16 12:20:15 +0800

    init: update card-list.jsx

---
+++ card-list.jsx
@@ -0,0 +1,99 @@
+function CardItemCell({ rows, column }) {
+  const value = dc.useMemo(() => column.value(rows), [rows, column.value]);
+  return <li style={{
+      overflowWrap: "break-word",
+      padding: "0",
+      margin: "0",
+    }}>
+      <dc.Literal value={value}></dc.Literal>
+    </li>;
+}
+
+function CardItem({ rows, columns }) {
+  const cols = dc.useMemo(
+    () => columns.map((column) => <CardItemCell rows={rows} column={column}></CardItemCell>)
+  , [rows, columns]);
+  return <ul style={{
+    listStyleType: "none",
+    border: "1px solid #ccc",
+    borderRadius: "5px",
+    padding: "10px"
+  }}>
+    {cols}
+  </ul>;
+}
+
+function CardList({ rows, columns, paging = undefined, onPageChange = (page) => { } }) {
+  if (rows.length === 0) {
+    return <div></div>;
+  }
+
+  if (paging <= 0) {
+    throw new Error("paging must be greater than 0");
+  }
+
+  const [current, setCurrent] = dc.useState(0);
+
+  const handlePageChange = dc.useCallback((page) => {
+    const pageNumber = Number(page); // fuck JavaScript
+    if (!isNaN(pageNumber)) {
+      setCurrent(pageNumber);
+      onPageChange(pageNumber);
+    }
+  }, [onPageChange]);
+
+  const rowsWithPaging = dc.useMemo(() => {
+    if (paging === undefined) {
+      return rows;
+    }
+    return rows.slice(current * paging, (current + 1) * paging);
+  }, [rows, current, paging]);
+
+  const cardItems = dc.useMemo(() => rowsWithPaging.map((row) => {
+    return <li style={{ padding: "0", margin: "0" }}>
+      <CardItem rows={row} columns={columns}></CardItem>
+    </li>;
+  }), [rowsWithPaging, columns]);
+
+  const options = dc.useMemo(() => {
+    if (paging === undefined) {
+      return [];
+    }
+    return new Array(Math.ceil(rows.length / paging))
+      .fill(0)
+      .map((_, i) => ({
+        value: i,
+        label: `Page ${i + 1}`
+      }));
+  }, [rows, paging]);
+
+  const Pageination = options.length > 1 && (
+    <dc.VanillaSelect
+      style={{ alignSelf: "flex-end" }}
+      options={options}
+      defaultValue={current}
+      onValueChange={handlePageChange}
+    />
+  );
+
+  return <div style={{
+    margin: "10px",
+    display: "flex",
+    flexDirection: "column",
+    gap: "10px"
+  }}>
+    <ul style={{
+      listStyleType: "none",
+      display: "grid",
+      gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
+      gap: "10px",
+      padding: "0",
+      margin: "0"
+    }}>
+      {cardItems}
+    </ul>
+    {Pageination}
+  </div>;
+}
+
+return { CardItemCell, CardItem, CardList };
